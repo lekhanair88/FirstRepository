b0f7585316c056699cf4f01b2db097298015b332
lekha.k.nair <lekha.k.nair@CDC2-L-71731JB.dir.svc.accenture.com>
2018-10-25 11:14:06 +0530

    FirstGitDemo pushed to git repository

---
+++ src/main/java/FirstGitDemo/GitDemo/App.java
@@ -0,0 +1,13 @@
+package FirstGitDemo.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/FirstGitDemo/GitDemo/GitDemoClass.java
@@ -0,0 +1,11 @@
+package FirstGitDemo.GitDemo;
+
+import org.testng.annotations.Test;
+
+public class GitDemoClass {
+  @Test
+  public void Demo() 
+  {
+	  System.out.println("Hello World !!");
+  }
+}
